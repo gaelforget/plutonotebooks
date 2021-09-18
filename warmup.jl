9a9f7626cef8a3cf69f5cf65d0d1f5538debe2e3
gaelforget <gforget@mit.edu>
2021-09-17 23:15:19 -0400

    add pkgs + revisit sysimage

--- warmup.jl
+++ warmup.jl
@@ -4,9 +4,14 @@ using Pluto
 using PlutoUI
 using StochasticDiffEq
 using UnicodePlots
+using Suppressor
 
-MC=MITgcm_config(configuration="global_with_exf")
+#MC=MITgcm_config(configuration="global_with_exf")
 #setup(MC)
 # build(MC,"--allow-skip")
 # launch(MC)
+
+tmp=ModelConfig(model=ClimateModels.RandomWalker)
+setup(tmp)
+launch(tmp)
 
